deef189a0e10f9bea6cd93bba8c68ca3238a07ff
gabrielstar <gabriel.starczewski@gmail.com>
2022-04-19 11:50:56 +0200

    embedded resources

--- 009_vars_executors_scenarios/02_http_get.js
+++ 009_vars_executors_scenarios/02_http_get.js
@@ -1,13 +1,26 @@
 import http from 'k6/http';
 
-export function setup() {
-    console.log('Setup')
-}
-export default function () { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
-    http.get('https://appxx.azurewebsites.net/');
-    console.log(`Test: this is user ${__VU}  and iter ${__ITER} iteration`)
+export let options = {
+    scenarios: {
+        admins: { //default
+            executor: 'shared-iterations',
+            vus: 1,
+            iterations: 1,
+            exec: 'admins'
+        },
+        users: { //default
+            executor: 'constant-vus',
+            vus: 5,
+            duration: '10s',
+            exec: 'users',
+        },
+    }
+};
 
+export function admins() { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
+    http.get('https://test.k6.io/contacts.php');
 }
-export function teardown() {
-    console.log('teardown')
+
+export function users() { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
+    http.get('https://appxx.azurewebsites.net/');
 }
